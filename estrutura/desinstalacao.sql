ef61da874986b5300c506e2ad0dcf93093f6c1b0
igorolivei <igorolivei@github.com>
2016-06-08 09:26:29 -0300

    Alteração para salvar usuário que lançou planilha e exibir no relatório

--- estrutura/desinstalacao.sql
+++ estrutura/desinstalacao.sql
@@ -3,3 +3,6 @@ drop sequence plugins.receitagrupo_sequencial_seq;
 
 drop table plugins.receitagrupoitem;
 drop sequence plugins.receitagrupoitem_sequencial_seq;
+
+DROP TABLE plugins.placaixausu;
+DROP SEQUENCE plugins.placaixausu_sequencial_seq;
